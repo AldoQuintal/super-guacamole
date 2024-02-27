39c4b261ffb3cd3f6d1d5ac12b597a8808732975
AldoQuintal <aldo.quintal@gasomarshal.mx>
2024-02-27 16:49:55 +0000

    Carga repo

---
+++ api/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Entregas
+
+# Register your models here.
+
+admin.site.register(Entregas)
---
+++ api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Entregas(models.Model):
+    vr_tanque = models.CharField(max_length=100)
+    fecha_ini = models.CharField(max_length=100)
+    fecha_fin = models.CharField(max_length=100)
+    vr_volumen = models.CharField(max_length=100)
+    vr_vol_ct = models.CharField(max_length=100)
+    vr_agua = models.CharField(max_length=100)
+    vr_temp = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+
