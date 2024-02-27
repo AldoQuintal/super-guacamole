c6cfb9eb6cfba359f773e440b02f547aad16490a
AldoQuintal <aldo.quintal@gasomarshal.mx>
2024-02-27 11:56:34 -0600

    Se añade tabla tanques al django

--- Tanques/admin.py
+++ Tanques/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Tanques
 
 # Register your models here.
+
+admin.site.register(Tanques)
--- Tanques/models.py
+++ Tanques/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Tanques(models.Model):
+
+    num_tanque = models.CharField(max_length=10)
+    producto = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=50)
+    capacidad = models.CharField(max_length=20)
+    altura = models.CharField(max_length=20)
+    vr_tanque = models.CharField(max_length=20)
+    inicia_entrega = models.CharField(max_length=20, default=False)
+    vol_ref = models.CharField(max_length=20)
+    fecha_ref = models.CharField(max_length=20)
+    vol_ct_ref = models.CharField(max_length=20)
