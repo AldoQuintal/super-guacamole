07edbf19e391bc7ad0db8fb65e931f4fe13468b9
AldoQuintal <aldo.quintal@gasomarshal.mx>
2024-02-27 12:08:45 -0600

    condicional

--- Tanques/models.py
+++ Tanques/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 class Tanques(models.Model):
 
-    num_tanque = models.CharField(max_length=10)
-    producto = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=50)
-    capacidad = models.CharField(max_length=20)
-    altura = models.CharField(max_length=20)
-    vr_tanque = models.CharField(max_length=20)
-    inicia_entrega = models.CharField(max_length=20, default=False)
-    vol_ref = models.CharField(max_length=20)
-    fecha_ref = models.CharField(max_length=20)
-    vol_ct_ref = models.CharField(max_length=20)
+    num_tanque = models.CharField(max_length=10, string="Número de Tanque")
+    producto = models.CharField(max_length=50, string="Producto")
+    descripcion = models.CharField(max_length=50, string="Clave Producto")
+    capacidad = models.CharField(max_length=20, string="Capacidad Total")
+    altura = models.CharField(max_length=20, string="Altura del tanque")
+    vr_tanque = models.CharField(max_length=20, invisible="1")
+    inicia_entrega = models.CharField(max_length=20, default=False, invisible="1")
+    vol_ref = models.CharField(max_length=20, invisible="1")
+    fecha_ref = models.CharField(max_length=20, invisible="1")
+    vol_ct_ref = models.CharField(max_length=20, invisible="1")
