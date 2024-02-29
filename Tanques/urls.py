8d0a084008a8b108d8621b020fe9a25addd69787
AldoQuintal <aldo.quintal@gasomarshal.mx>
2024-02-29 16:02:11 -0600

    Editar tanque

--- Tanques/urls.py
+++ Tanques/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
-from Tanques.views import TanquesListView, eliminar_tanque, registrar_tanque, edit_tanque
+from Tanques.views import TanquesListView, eliminar_tanque, registrar_tanque, edit_tanque, editar_tanque
 
 
 urlpatterns = [
     path('', TanquesListView.as_view(), name='gestion_tanques'),
     path('registrarTanque/', registrar_tanque),
     path('eliminacionTanques/<int:id>', eliminar_tanque ),
-    path('edicionTanques/<int:id>', edit_tanque )
+    path('edicionTanques/<int:id>', edit_tanque ),
+    path('editarTanque/', editar_tanque )
 ]
 
 
--- Tanques/views.py
+++ Tanques/views.py
@@ -53,3 +53,13 @@ def edit_tanque(request, id):
 
     return render(request, "edicionTanque.html", data)
 
+
+def editar_tanque(request):
+    num_tanque=request.POST['txtnum_tanque']
+    producto=request.POST['txtprodcuto']
+    description=request.POST['txtdescripcion']
+    capacidad=request.POST['txtcapacidad']
+    altura=request.POST['txtaltura']
+
+    #tanque =Tanques.objects.create(num_tanque=num_tanque, producto=producto, descripcion=description, capacidad=capacidad, altura=altura)
+    return redirect('/')
