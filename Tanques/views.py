11508f2cd9a61a24a106a3021c6fda1a25e9024d
AldoQuintal <aldo.quintal@gasomarshal.mx>
2024-02-29 16:08:44 -0600

    edición completada

--- Tanques/views.py
+++ Tanques/views.py
@@ -55,11 +55,20 @@ def edit_tanque(request, id):
 
 
 def editar_tanque(request):
+    id = int(request.POST['id'])
     num_tanque=request.POST['txtnum_tanque']
     producto=request.POST['txtprodcuto']
     description=request.POST['txtdescripcion']
     capacidad=request.POST['txtcapacidad']
     altura=request.POST['txtaltura']
+
+    tanque=Tanques.objects.get(id=id)
+    tanque.num_tanque = num_tanque
+    tanque.producto = producto
+    tanque.descripcion = description
+    tanque.capacidad = capacidad
+    tanque.altura = altura
+    tanque.save()
 
     #tanque =Tanques.objects.create(num_tanque=num_tanque, producto=producto, descripcion=description, capacidad=capacidad, altura=altura)
     return redirect('/')
