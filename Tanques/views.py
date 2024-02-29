af590d27316ecbc01fcb48fbc7767c556cda08b5
AldoQuintal <aldo.quintal@gasomarshal.mx>
2024-02-29 09:50:03 -0600

    editar, eliminar registro

--- Tanques/views.py
+++ Tanques/views.py
@@ -1,4 +1,6 @@
 #from django.http import HttpResponse
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Tanques
@@ -16,3 +18,11 @@ def home(request):
 class TanquesListView(ListView):
     model=Tanques
     template_name='tanques_view.html'
+
+    def get_queryset(self):       
+        return Tanques.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['titulo'] = 'Regitro de Tanques'
+        return context
