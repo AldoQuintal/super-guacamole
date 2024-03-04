adca3d66b2545802dc4d82d213c57515828b06d3
AldoQuintal <aldo.quintal@gasomarshal.mx>
2024-03-04 10:00:14 -0600

    number smallint

--- Tanques/models.py
+++ Tanques/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Tanques(models.Model):
 
-    num_tanque = models.CharField(max_length=10, verbose_name="Número de Tanque")
+    num_tanque = models.SmallIntegerField(verbose_name="Número de Tanque")
     producto = models.CharField(max_length=50, verbose_name="Producto")
     descripcion = models.CharField(max_length=50, verbose_name="Clave Producto")
     capacidad = models.CharField(max_length=20, verbose_name="Capacidad Total")
