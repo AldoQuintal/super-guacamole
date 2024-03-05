b7804499d2b208ea641cfe04a541af596f636f4e
AldoQuintal <aldo.quintal@gasomarshal.mx>
2024-03-05 16:03:57 -0600

    reverse_lazy

--- RestFramework/settings.py
+++ RestFramework/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse_lazy
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +30,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 LOGIN_REDIRECT_URL = '/tanques/'
-LOGOUT_REDIRECT_URL = '/login/'
+LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
 
 
