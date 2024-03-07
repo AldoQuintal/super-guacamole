4bd01db4d54ff018132f15d158e1d17e5f7562ea
AldoQuintal <aldo.quintal@gasomarshal.mx>
2024-03-07 10:06:22 -0600

    /

--- Tanques/urls.py
+++ Tanques/urls.py
@@ -24,10 +24,10 @@ urlpatterns = [
 
     path('registroTablaCubicaje/', registro_puntos),
 
-    path('registroTablaCubicajeT1/', registro_puntos),
-    path('registroTablaCubicajeT2/', registro_puntos),
-    path('registroTablaCubicajeT3/', registro_puntos),
-    path('registroTablaCubicajeT4/', registro_puntos),
+    path('registro/TablaCubicajeT1/', registro_puntos),
+    path('registro/TablaCubicajeT2/', registro_puntos),
+    path('registro/TablaCubicajeT3/', registro_puntos),
+    path('registro/TablaCubicajeT4/', registro_puntos),
 
     path('tanques/tablaCubicaje/eliminacionPuntos/<int:id_rex>', delete_punto),
     path('accounts/register/', register),
