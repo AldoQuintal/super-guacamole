36e87ab89a4947031249f3017daf45b5d74b1d71
AldoQuintal <aldo.quintal@gasomarshal.mx>
2024-03-07 12:18:35 -0600

    Tanques.urls

--- RestFramework/urls.py
+++ RestFramework/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include('api.urls', 'Tanques.urls')),
     path('', include('Tanques.urls')),
     path('configuracion/', include('Tanques.urls')),
     path('accounts/', include('django.contrib.auth.urls'))
