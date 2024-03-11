161979a2bd9fd84f6b7da83648a8a9c22ebf373a
AldoQuintal <aldo.quintal@gasomarshal.mx>
2024-03-11 11:21:06 -0600

    console log

--- Tanques/static/js/gestionTanques.js
+++ Tanques/static/js/gestionTanques.js
@@ -3,6 +3,7 @@ const btnsEliminacion=document.querySelectorAll('.btnEliminacion');
 (function () {
     
     btnsEliminacion.forEach(btn => {
+        console.log(btn)
         btn.addEventListener('click', function(e) {
             let confirmacion=confirm("Borrado prro");
             if (!confirmacion){
