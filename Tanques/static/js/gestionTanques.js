ccbe9b6744593a610caed919f47fd69b1b43dbf7
AldoQuintal <aldo.quintal@gasomarshal.mx>
2024-03-11 11:09:08 -0600

    confirmar

--- Tanques/static/js/gestionTanques.js
+++ Tanques/static/js/gestionTanques.js
@@ -3,7 +3,12 @@ const btnsEliminacion=document.querySelectorAll('.btnEliminacion');
 (function () {
     
     btnsEliminacion.forEach(btn => {
-        console.log(btn);
+        btn.addEventListener('click', function(e) {
+            let confirmacion=confirm("Borrado prro");
+            if (!confirmacion){
+                e.preventDefault();
+            }
+        })
     })
 
 })();
